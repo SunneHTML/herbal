ef70091d9130797d1bfaae5ecd7f2371ca98ac62
SunneHTML <divaworksone@gmail.com>
2026-05-18 19:45:31 -0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <div style={{
+      padding: '40px',
+      fontFamily: 'Arial',
+      background: 'white',
+      color: 'black'
+    }}>
+      <h1>Herbal Artista</h1>
+      <p>The React app is working.</p>
+    </div>
+  )
+}
